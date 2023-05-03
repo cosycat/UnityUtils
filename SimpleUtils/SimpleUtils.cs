461b86400aab614f586ebffad26149da23934e60
Flavia Brogle <flavia.brogle@fhnw.ch>
2023-05-03 09:32:16 +0200

    Command Line Args Getter

---
+++ SimpleUtils/SimpleUtils.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace UnityUtils.SimpleUtils
+{
+    public class SimpleUtils
+    {
+        public static Dictionary<string, string> GetCommandlineArgs()
+        {
+            Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+            var args = System.Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < args.Length; ++i)
+            {
+                var arg = args[i].ToLower();
+                if (arg.StartsWith("-"))
+                {
+                    var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+                    value = (value?.StartsWith("-") ?? false) ? null : value;
+
+                    argDictionary.Add(arg, value);
+                }
+            }
+            return argDictionary;
+        }
+    }
+}
